b349b9bad114a577d5b819fb5b7fe2a5ed654576
pvoegele1 <patrick_voegele@web.de>
2020-03-22 15:56:33 +0100

    tut 2 Übungspush

---
+++ bw240_20_1/src/de/hs_lu/o2s/tut2/uebung/Autoliste.java
@@ -0,0 +1,24 @@
+package de.hs_lu.o2s.tut2.uebung;
+
+public class Autoliste {
+
+	Auto[] autoA;
+
+	public Autoliste() {
+		autoA = new Auto[0];
+	}
+
+	public Auto[] getAutoA() {
+		return autoA;
+	}
+
+	public void setAutoA(Auto[] autoA) {
+		this.autoA = autoA;
+	}
+
+	/**
+	 * Bitte ab hier bearbeiten!
+	 */
+
+	
+}
