377d0deb0b491125b70b231ce1365b6fd848a996
pvoegele1 <patrick_voegele@web.de>
2020-04-14 09:40:19 +0200

    push

--- bw240_20_1/src/de/hs_lu/o2s/tut4/uebung/Fahrzeugliste.java
+++ bw240_20_1/src/de/hs_lu/o2s/tut4/uebung/Fahrzeugliste.java
@@ -22,43 +22,65 @@ public class Fahrzeugliste {
 		Fahrzeug[] copyArray = new Fahrzeug[this.getFahrzeugList().length + 1];
 		System.arraycopy(this.getFahrzeugList(), 0, copyArray, 0, this.getFahrzeugList().length);
 		copyArray[this.getFahrzeugList().length + 1] = f;
-		
-		//this.fahrzeugList = copyArray;
+
+		// this.fahrzeugList = copyArray;
 		this.setFahrzeugList(copyArray);
-		System.out.println("Folgendes Fahrzeug wurde der Fahrzeugliste hinzugefügt: "+f.toString());
+		System.out.println("Folgendes Fahrzeug wurde der Fahrzeugliste hinzugefügt: " + f.toString());
 		return true;
 	}
-	
-	
+
 	public void addFahrzeugListe(Fahrzeugliste fL) {
 		for (int i = 0; i < fL.getFahrzeugList().length; i++) {
 			this.addFahrzeug(fL.getFahrzeugList()[i]);
 		}
 	}
-	
+
 	public boolean delFahrzeug(int pos) {
-		
+
 		Fahrzeug merkFahrzeug = this.getFahrzeugList()[pos];
-		//[][][X][][]
-		
-		Fahrzeug[] copyArray = new Fahrzeug [this.getFahrzeugList().length - 1];
-		
-		
-		
-		
-		
-		
-		System.out.println("Folgendes Fahrzeug wurde aus der Fahrzeugliste entfernt: "+merkFahrzeug.toString());
-		
+		// this.getFahrzeugliste()
+		// X
+		// [0][1][2][3][4]
+		//
+		// copyArray
+		// [0][1][2][3]
+		Fahrzeug[] copyArray = new Fahrzeug[this.getFahrzeugList().length - 1];
+
+		// Kopieren der Elemente bis zur Position die wir übergeben bekommen haben.
+		System.arraycopy(this.getFahrzeugList(), 0, copyArray, 0, pos);
+		// Kopieren der Elemente ab Position + 1, weil wir das merkerFahrzeug
+		// überspringen möchten.
+		System.arraycopy(this.getFahrzeugList(), pos + 1, copyArray, pos, this.getFahrzeugList().length - pos);
+
+		System.out.println("Folgendes Fahrzeug wurde aus der Fahrzeugliste entfernt: " + merkFahrzeug.toString());
+		return true;
+	}
+
+	public boolean checkPKW() {
+		for (int i = 0; i < this.getFahrzeugList().length; i++) {
+			//Prüfung, ob eine Instanz von PKW in der Fahrzeugliste beinhaltet ist. Falls ja, wird true zurückgegeben
+			if (this.getFahrzeugList()[i] instanceof PKW) {
+				return true;
+			}
+		}
+		//Falls nicht, wird false returned.
+		return false;
 	}
 	
-	
+	//Entfernen aller PKW im Array.
+	public boolean delPKW_All() {
+		//Iteration über alle PKWs
+		for (int i = 0; i < this.getFahrzeugList().length; i++) {
+			//Prüfung, ob aktuelles Element Instanz von PKW
+			if(this.getFahrzeugList()[i] instanceof PKW) {
+				//Wenn ja, nutze Index und delFahrzeug Methode, um Fahrzeug aus dem Array zu entfernen.
+				this.delFahrzeug(i);
+			}
+		}
+		return true;
+		
+	}
 
-	
-	
-	
-	
-	
 	
 	
 	
