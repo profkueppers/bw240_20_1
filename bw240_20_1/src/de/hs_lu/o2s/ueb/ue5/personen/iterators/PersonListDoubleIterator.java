0f66c474c392b45a69b4e5b6da5ca636dab36548
Dennis Messer <dm@innofabrik.de>
2020-04-25 10:01:26 +0200

    UE 5

---
+++ bw240_20_1/src/de/hs_lu/o2s/ueb/ue5/personen/iterators/PersonListDoubleIterator.java
@@ -0,0 +1,57 @@
+package de.hs_lu.o2s.ueb.ue5.personen.iterators;
+
+import java.util.Iterator;
+
+// ACHTUNG: Nur in dieser Klasse müssen Sie etwas ändern
+// Ziel: hasNext und next sollen sinnvolle Rückgabewerte haben, damit man mit der erweiterten For-Schleife über PesonLists iterieren (also über jedes Element zweimal "gehen") kann
+
+public class PersonListDoubleIterator implements Iterator {
+	
+	
+			
+	public PersonListDoubleIterator(PersonList persList) {
+			
+	}
+
+	public boolean hasNext() {
+		return false; // Achtung: dies ist eine Standardrückgabe und noch falsch! Hier muss die Logik implementiert werden.
+	}
+
+	public Object next() {
+		return null; // Achtung: dies ist eine Standardrückgabe und noch falsch! Hier muss die Logik implementiert werden.
+	}
+
+	
+	
+	
+}
+
+=======
+package de.hs_lu.o2s.ueb.ue5.personen.iterators;
+
+import java.util.Iterator;
+
+// ACHTUNG: Nur in dieser Klasse müssen Sie etwas ändern
+// Ziel: hasNext und next sollen sinnvolle Rückgabewerte haben, damit man mit der erweiterten For-Schleife über PesonLists iterieren (also über jedes Element zweimal "gehen") kann
+
+public class PersonListDoubleIterator implements Iterator {
+	
+	
+			
+	public PersonListDoubleIterator(PersonList persList) {
+		
+	
+	}
+
+	public boolean hasNext() {
+		return false; // Achtung: dies ist eine Standardrückgabe und noch falsch! Hier muss die Logik implementiert werden.
+	}
+
+	public Object next() {
+		return null; // Achtung: dies ist eine Standardrückgabe und noch falsch! Hier muss die Logik implementiert werden.
+	}
+
+	
+	
+	
+}
