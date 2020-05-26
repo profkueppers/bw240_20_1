f916a1db22723b42b247779dc6f0f16b6e436592
pvoegele1 <patrick_voegele@web.de>
2020-05-26 07:38:01 +0200

    upload

--- bw240_20_1/src/de/hs_lu/o2s/tutorium/exception/loesung/NichtAltGenugException.java
+++
@@ -1,8 +0,0 @@
-package de.hs_lu.o2s.tutorium.exception.loesung;
-
-public class NichtAltGenugException extends KeinFuehrerscheinException {
-	
-	public NichtAltGenugException(String msg) {
-		super(msg);
-	}
-}
